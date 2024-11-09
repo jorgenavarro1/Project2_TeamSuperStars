741ecc644a09389396baa52284b5d8ac1fe7bde0
azki4 <156265103+azki4@users.noreply.github.com>
2024-11-09 02:47:25 -0600

    changes

--- css/script.js
+++ css/script.js
@@ -2,13 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const filterButtons = document.querySelectorAll('.filter-buttons button');
     const menuItems = document.querySelectorAll('.menu-item');
 
+    // Display all items by default
+    menuItems.forEach(item => item.style.display = 'block');
+
     filterButtons.forEach(button => {
         button.addEventListener('click', () => {
             const filter = button.getAttribute('data-filter');
 
             // Apply filtering logic
             menuItems.forEach(item => {
-                const categories = item.getAttribute('data-category');
+                const categories = item.getAttribute('data-category').split(' ');
                 if (filter === 'all' || categories.includes(filter)) {
                     item.style.display = 'block';
                 } else {
